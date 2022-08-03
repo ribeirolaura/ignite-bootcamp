90980be06dd13f52c41e6802e8ebc59e3b5a527e
unknown <lauradeor@gmail.com>
2022-08-03 20:44:27 -0300

    feat: add new http client and configure miragejs as mock server

--- dt-monet/src/components/TransactionalTable/index.tsx
+++ dt-monet/src/components/TransactionalTable/index.tsx
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
+import { api } from "../../services/api";
 import { Container } from "./styles";
 
 export function TransactionalTable(){
+  useEffect(() => {
+    api.get('transactions')
+      .then(response => {
+        console.log(response.data)
+      })
+
+  }, [])
+
 
   return(
     <Container>
