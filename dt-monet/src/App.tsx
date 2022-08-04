ff1fa37aac14514e6c702f6eca519900ce34c26b
unknown <lauradeor@gmail.com>
2022-08-04 19:09:24 -0300

    feat: add react-modal library and Modal components to the app

--- dt-monet/src/App.tsx
+++ dt-monet/src/App.tsx
@@ -2,11 +2,38 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global"
 
+import Modal from 'react-modal';
+import { useState } from "react";
+
+// Para facilitar em quesitos de acessibilidade, antes da declaração abaixo, o modal envolvia a div #root principal gerada no html e levando em consideração a acessibilidade
+// para leitura dos elementos, o elemento #root deve ser o principal a envolver todo o código
+// source: doc react-modal  
+Modal.setAppElement('#root');
+
 export function App() {
+
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false); 
+
+  
+  function handleOpenNewTransactionModal(){
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal(){
+    setIsNewTransactionModalOpen(false);
+
+  }
+  
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <Modal 
+          isOpen={isNewTransactionModalOpen}
+          onRequestClose={handleCloseNewTransactionModal}
+        >
+          <h2>Cadastro de Modal</h2>
+        </Modal>
       <GlobalStyle />
     </>
   );
